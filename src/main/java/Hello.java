8bfcce04871c97d93c6282795ccecde55db2f4e4
Michał Gawidziel <michal.gawidziel@nn.pl>
2022-05-27 18:46:14 +0200

    asd

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -12,6 +12,7 @@ public class Hello extends HttpServlet {
    public void init() throws ServletException {
       // Do required initialization
       message = "Hello World: src/main/java/Hello.java";
+      System.out.println("BUILD: " +System.getEnv("BUILD_TIME"));
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
